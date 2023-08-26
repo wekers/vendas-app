0c569b818e077e491411b73a04024cd2f77c16ab
wekers <fernando@wekers.org>
2023-08-26 17:30:42 -0300

    Method atualizar (update) produto

--- backend/vendas-app/src/main/java/io/github/wekers/vendasapp/rest/produtos/ProdutoController.java
+++ backend/vendas-app/src/main/java/io/github/wekers/vendasapp/rest/produtos/ProdutoController.java
@@ -3,7 +3,12 @@ package io.github.wekers.vendasapp.rest.produtos;
 import io.github.wekers.vendasapp.model.Produto;
 import io.github.wekers.vendasapp.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -21,6 +26,22 @@ public class ProdutoController {
         repository.save(entidadeProduto);
         return ProdutoFormRequest.fromModel(entidadeProduto);
 
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto){
+        Optional<Produto> produtoExistente = repository.findById(id);
+
+        if(produtoExistente.isEmpty()){
+            return ResponseEntity.notFound().build();
+            //throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        Produto entidade = produto.toModel();
+        entidade.setId(id);
+        repository.save(entidade);
+
+        return ResponseEntity.ok().build();
 
     }
 }
